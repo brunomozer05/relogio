4811672ef73a3cec024a21a4b0a85ffa452beebb
unknown <brunomozer05@gmail.com>
2024-11-03 12:35:43 -0300

    Formatção do codigo e alinhamento do do Relógio

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function atualizarRelogio() {
 
-const dia = new Date().getDay()
-const hora = String(new Date().getHours()).padStart(2, '0');
-const minuto = String(new Date().getMinutes()).padStart(2, '0');
-const segundo = String(new Date().getSeconds()).padStart(2, '0');
-const milissegundo = String(new Date().getMilliseconds()).padStart(2, '0');
+    const dia = new Date().getDay()
+    const hora = String(new Date().getHours()).padStart(2, '0');
+    const minuto = String(new Date().getMinutes()).padStart(2, '0');
+    const segundo = String(new Date().getSeconds()).padStart(2, '0');
+    const milissegundo = String(new Date().getMilliseconds()).padStart(2, '0');
 
-const horario = `${hora}:${minuto}:${segundo}.${milissegundo}`
-document.getElementById('relogio').textContent = horario;
+    const horario = `${hora}:${minuto}:${segundo}.${milissegundo}`
+    document.getElementById('relogio').textContent = horario;
 }
 atualizarRelogio();
 setInterval(atualizarRelogio, 10);
